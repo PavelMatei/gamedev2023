e6f004a28c36ff661fcc04f5e97e7abcacd3e2f7
Frontend JavaScript <frontendjavascript@C3-22.local>
2023-07-18 21:03:57 +0300

    cursgame

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gamedev2023
+<!-- https://www.guru99.com/file-permissions.html -->
+<!-- https://thorbjorn.itch.io/tiled/download/eyJleHBpcmVzIjoxNjg5MDk0ODQ2LCJpZCI6Mjg3Njh9.btg3FwgLkee0bTkEzKV%2fn%2f%2f4ilE%3d -->
+<!-- https://animate.style/ -->
+<!-- https://vuejs.org/guide/quick-start.html#using-vue-from-cdn -->
+<!-- https://developer.mozilla.org/en-US/ -->
+<!-- https://getbootstrap.com/docs/4.0/components/forms/ -->
+<!-- https://animejs.com/documentation/#JSobject -->
+<!-- https://www.datainfinities.com/ -->
---
+++ curs_04/memory/src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+import Tile from './Tile';
+
+function shuffle(tiles){
+  for(let i=tiles.length -1;i>0;i--){
+    const j=Math.floor (Math.random()*tiles.length);//[0;1)
+    console.log('exchange'+i+'with'+j);
+    [tiles[i],tiles[j]]=[tiles[j],tiles[i]]
+  }
+  console.log(tiles);
+}
+function App() {
+  let objects=['bear','corgi','crab','fish','panda','parrot','pig','rabbit','rhinoceros','shark','snail','turtle'];
+  objects=[...objects, ...objects];
+  // const array1=[1,4,9,16];
+  let a=5;
+  let b=6;
+  [a,b]=[b,a];
+  console.log(a,b)
+  shuffle(objects)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className='underline'>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div className='grid grid-cols-6 gap-4 justify-items-center'>
+        {objects.map(function(object, index){
+          return <Tile 
+            tile={object}
+            altText={'icon of '+object}
+            culoare={'rosu'}
+            index={index} />
+        })}
+        {/* {array1.map(x=>x*2+'')}
+        {array1.map(function(x){
+          return x*2;
+        })} */}
+        {/* {objects.forEach(function(object){
+          console.log(object,' in foreach');
+          return <div>{object}</div>
+        })} */}
+      </div>
+    </div>
+  )
+}
+
+export default App;
